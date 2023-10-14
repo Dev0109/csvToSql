2c1dfb0aba708a6be38d8c4bdc7b4873db38f242
syeda0109 <syed.new.website@gmail.com>
2023-10-14 15:04:08 +0300

    update

--- import.php
+++ import.php
@@ -3,25 +3,56 @@
 if (isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] == UPLOAD_ERR_OK) {
     $file = $_FILES['csv_file']['tmp_name'];
 
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "aca1_db1";
+
+    // Create a database connection
+    $mysqli = new mysqli($hostname, $username, $password, $database);
+
+    // Check the connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    $tableName = "p004_NMI_Detail";
+
     $csvFile = $file;
+    $columnsName = [];
     $isFirstRow = true;
-    
-    if (($handle = fopen($csvFile, "r")) !== false) {        
+    $createTableSQL = "CREATE TABLE IF NOT EXISTS $tableName (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+    )";
+    $mysqli->query($createTableSQL);
+
+    if (($handle = fopen($csvFile, "r")) !== false) {
         while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-            echo(count($data));
-            die();
+            $len = count($data);
 
             if ($isFirstRow) {
                 $isFirstRow = false;
+                for ($i = 0; $i < $len; $i++) {
+                    // Collect the column names from the CSV file
+                    $data_value[$i] = str_replace(' ', '_', $data[$i]);
+                    $columnsName[$i] = $data_value[$i];
 
-                continue; // Skip the rest of the loop and move to the next row
+                    // Generate ALTER TABLE statements to add these columns to the table
+                    $sql = "ALTER TABLE $tableName ADD {$data_value[$i]} VARCHAR(255)";
+                    
+                    if ($mysqli->query($sql) === TRUE) {
+                        echo "Column '{$data_value[$i]}' added successfully.";
+                    } else {
+                        echo "Error adding column '{$data_value[$i]}': " . $mysqli->error;
+                    }
+                }
             }
-            
-            
-            $stmt->close();
         }
-        
-        // Close the CSV file
+
         fclose($handle);
     }
+
+    // Close the database connection
+    $mysqli->close();
 }
+?>
